c73321d29b6b9962b5af028c2a1c6337320ceb59
Jay D <BlitzAction@users.noreply.github.com>
2016-12-16 11:51:14 -0500

    Add files via upload

---
+++ server.R
@@ -0,0 +1,25 @@
+# server.R
+
+library(quantmod)
+source("helpers.R")
+
+shinyServer(function(input, output) {
+  
+  dataInput <- reactive({
+    getSymbols(input$symb, src = "yahoo", 
+               from = input$dates[1],
+               to = input$dates[2],
+               auto.assign = FALSE)
+
+  })
+  finalData <- reactive({ 
+      if (input$adjust)  (return(adjust(dataInput())))
+      else( return(dataInput()))
+        })
+    output$plot <- renderPlot({   
+   
+    
+    chartSeries(finalData(), theme = chartTheme("white"), 
+                type = "line", log.scale = input$log, TA = NULL)
+  })
+})
